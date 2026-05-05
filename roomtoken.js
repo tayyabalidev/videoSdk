e279949a9948c6a6d12c0fb3091564cf4b6eeda7
Tayyab ali <tayyabali.02460@gmail.com>
2026-05-05 20:13:14 +0500

    change the name

--- roomtoken.js
+++ roomtoken.js
@@ -200,7 +200,7 @@ module.exports = async ({ req, res, log }) => {
       });
     }
 
-    // POST: create room + token atomically
+    // POST: creates room + token atomically
     const createdMeetingId = await createRoom(apiKey, secretKey);
     const token = buildMeetingToken({
       apiKey,
